c5fd9ee484ce48380cb277eaaa81fe317ba3d5b3
theproductcoach <will@productcoach.io>
2025-04-09 15:38:03 +0100

    Initial setup for Nurtura app

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,7 +23,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className="light">
+      <head>
+        <meta name="color-scheme" content="light" />
+      </head>
       <body
         suppressHydrationWarning
         className={`${geistSans.variable} ${geistMono.variable}`}
