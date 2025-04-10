1e175e9074ad36a94dbb880cdb942ac0e9c9a278
theproductcoach <will@productcoach.io>
2025-04-10 11:28:57 +0100

    fixing linter

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -2,7 +2,6 @@
 
 import { useState } from "react";
 import { useRouter } from "next/navigation";
-import Image from "next/image";
 import styles from "./page.module.css";
 
 type Role = "family" | "carer" | null;
